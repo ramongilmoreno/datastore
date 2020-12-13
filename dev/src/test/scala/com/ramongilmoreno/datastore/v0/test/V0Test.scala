19b8d30605c4bf98feae9d9f5f103dddebf9c086
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2020-12-13 19:04:43 +0100

    More parser tests

--- dev/src/test/scala/com/ramongilmoreno/datastore/v0/test/V0Test.scala
+++ dev/src/test/scala/com/ramongilmoreno/datastore/v0/test/V0Test.scala
@@ -5,7 +5,21 @@ import com.ramongilmoreno.datastore.v0.implementation.QueryParser._
 import org.scalatest._
 
 class V0Test extends FlatSpec {
-  "Parser" should "parse a simple query" in {
+
+  "Parser" should "parse a query without conditions" in {
+    val query = "select a, b from c"
+    val result = Query(
+      List(
+        Field("a"),
+        Field("b")
+      ),
+      "c",
+      None
+    )
+    assert(QueryParser.parse(query).get == result)
+  }
+
+  it should "parse a query with several conditions" in {
     val query =
       """
         |select
@@ -19,7 +33,6 @@ class V0Test extends FlatSpec {
         |    "f" <> g or
         |    h = "i"
         |  )
-        |
         |""".stripMargin
     val result = Query(
       List(
@@ -34,6 +47,32 @@ class V0Test extends FlatSpec {
             SingleCondition(Value("f"), NotEqual, Field("g")),
             SingleCondition(Field("h"), Equal, Value("i")),
           )
+        )
+      )
+    )
+    assert(QueryParser.parse(query).get == result)
+  }
+
+  it should "parse a query just with OR" in {
+    val query =
+      """
+        |select
+        |  a
+        |from
+        |  b
+        |where
+        |  c = d or
+        |  e = f
+        |""".stripMargin
+    val result = Query(
+      List(
+        Field("a"),
+      ),
+      "b",
+      Some(
+        OrCondition(
+          SingleCondition(Field("c"), Equal, Field("d")),
+          SingleCondition(Field("e"), Equal, Field("f"))
         )
       )
     )
